58365629836cf03aa56729cab229f7d8283b3e1b
Ayberk ESER <cryonayes@gmail.com>
2021-06-21 13:38:00 +0300

    Add files via upload

--- GoFilter.go
+++ GoFilter.go
@@ -10,17 +10,17 @@ import (
 )
 
 type URLData struct {
-	Host string
-	Path string
-	FullURL string
-	ParameterSize int
+	Host           string
+	Path           string
+	FullURL        string
+	ParameterSize  int
 	ParameterCount int
 }
 
 type Config struct {
-	quiet bool
-	filename string
-	paramCount bool
+	quiet         bool
+	filename      string
+	forLength     bool
 	minParamCount int
 }
 
@@ -35,7 +35,6 @@ func readFile(config Config) {
 	readAll := bufio.NewScanner(file)
 
 	processedURLS := make(map[string]URLData)
-	prevURL := URLData{}
 
 	for readAll.Scan() {
 		currentLine := readAll.Text()
@@ -44,30 +43,27 @@ func readFile(config Config) {
 		if parseError != nil {
 			continue
 		}
-
+		currentPath := currentURL.Host + currentURL.Path
 		parameters, _ := url.ParseQuery(currentURL.RawQuery)
 
-		if processedURLS[currentURL.Host] == (URLData{}) {
-			processedURLS[currentURL.Host] = URLData{
-				Host:     currentURL.Host,
-				Path:     currentURL.RawQuery,
-				FullURL:  currentURL.Scheme+"://"+ currentURL.Host + currentURL.Path + currentURL.RawQuery,
-				ParameterSize: len(currentURL.RawQuery),
-				ParameterCount : len(parameters),
-			}
+		currentURLData := URLData{
+			Host:           currentURL.Host,
+			Path:           currentURL.RawQuery,
+			FullURL:        currentLine,
+			ParameterSize:  len(currentURL.RawQuery),
+			ParameterCount: len(parameters),
 		}
 
-		if config.paramCount {
-			if prevURL.Host == processedURLS[currentURL.Host].Host && prevURL.ParameterCount > processedURLS[currentURL.Host].ParameterCount {
-				processedURLS[currentURL.Host] = prevURL
-			}
-		}else {
-			if prevURL.Host == processedURLS[currentURL.Host].Host && prevURL.ParameterSize > processedURLS[currentURL.Host].ParameterSize {
-				processedURLS[currentURL.Host] = prevURL
-			}
+		if processedURLS[currentPath] == (URLData{}) {
+			processedURLS[currentPath] = currentURLData
+			continue
 		}
 
-		prevURL = processedURLS[currentURL.Host]
+		if config.forLength && processedURLS[currentPath].ParameterSize < len(currentURL.RawQuery) {
+			processedURLS[currentPath] = currentURLData
+		} else if !config.forLength && processedURLS[currentPath].ParameterCount < len(parameters) {
+			processedURLS[currentPath] = currentURLData
+		}
 	}
 
 	for _, data := range processedURLS {
@@ -84,15 +80,16 @@ func readFile(config Config) {
 }
 
 func main() {
+
 	flag.Usage = func() {
 		help := []string{
 			"Filter given URLs based on query size",
 			"",
 			"Options:",
-			"  -f,  --file			File to process",
-			"  -c,  --count			Run analysis based on query count, otherwise query length",
-			"  -mc, --min-count		Minimum parameter count",
-			"  -q,  --quiet			Only print fullpath",
+			"  -f,  --file          File to process",
+			"  -l,  --length        Run analysis based on query length",
+			"  -mc, --min-count     Minimum parameter count",
+			"  -q,  --quiet         Only print fullpath",
 			"",
 		}
 		_, _ = fmt.Fprintf(os.Stderr, strings.Join(help, "\n"))
@@ -106,8 +103,8 @@ func main() {
 	flag.BoolVar(&mConfig.quiet, "quiet", false, "")
 	flag.BoolVar(&mConfig.quiet, "q", false, "")
 
-	flag.BoolVar(&mConfig.paramCount, "count", false, "")
-	flag.BoolVar(&mConfig.paramCount, "c", false, "")
+	flag.BoolVar(&mConfig.forLength, "length", false, "")
+	flag.BoolVar(&mConfig.forLength, "l", false, "")
 
 	flag.IntVar(&mConfig.minParamCount, "min-count", 1, "")
 	flag.IntVar(&mConfig.minParamCount, "mc", 1, "")
